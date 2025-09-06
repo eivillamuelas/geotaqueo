a782dbfec9ae7abb7e888b6e73fc5c7294a1b5eb
ei <eivillamuelas@gmail.com>
2025-09-06 13:37:26 +0200

    Añadiendo geolocalización, layouts y about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-      site: 'https://eivillamuelas.github.io/geotaqueo/',
+    site: "https://eivillamuelas.github.io/geotaqueo/",
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
